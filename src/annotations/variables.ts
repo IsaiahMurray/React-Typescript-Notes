37a187c0dcdc89bc6bac2136d411887be2725429
Isaiah Murray <murray.isaiah13@gmail.com>
2020-09-21 21:38:02 -0400

    update notes

--- src/annotations/variables.ts
+++ src/annotations/variables.ts
@@ -1,5 +1,8 @@
 //TYPE ANNOTATIONS
-//has to be assigned the datatype it was given
+
+//In type annotation, the developer tells the code what the datatype should be 
+//and it has to be assigned the datatype it was given
+
 let apples: number = 5;
 
 let speed: string = 'fast';
@@ -32,3 +35,31 @@ let point: { x: number; y: number; }= {
     x: 10,
     y: 20
 };
+
+//Function
+//after logNumber, we put what we expect the function to take in, then put out
+//so it will take in a number(being the variable i), then output nothing(so that will be void)
+
+//---------------+annotation-------+---+implementation
+const logNumber: (i: number) => void = (i: number) => {
+    console.log(i);
+}
+
+//Type Inferance
+//This is when we don't describe the datatype and instead we let typscript figure it out
+//It requires less code and will be used in most cases
+let oranges = 5;
+
+
+//When to use annotations
+
+//-When we want a variable to  have a type that can't be inferred
+//-When a funciton returns the 'any' type and we need to clarify the value
+const json = '{"x": 10, "y": 20}';
+const coordinates = JSON.parse(json);
+console.log(coordinates);
+
+
+//-When we declare a variable on one line then initialize it later
+let words = ['red', 'green', 'blue'];
+let foundWord;
